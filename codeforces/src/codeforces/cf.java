2f38ce724138879badd8871a95297456fea77fbd
Bhrugu Dave <rangbhrugu@outlook.com>
2020-11-24 18:34:03 +0530

    standard template

---
+++ codeforces/src/codeforces/cf.java
@@ -0,0 +1,16 @@
+package codeforces;
+import java.util.*;
+import java.io.*;
+
+public class cf {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		while(t-- > 0)
+		{
+			
+		}
+	}
+}
